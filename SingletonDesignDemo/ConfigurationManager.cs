4d41df6ff8c41e8abb07a47659385e556aa2af55
Vinod Philip <135483551+vinodp297@users.noreply.github.com>
2023-07-31 21:01:06 -0700

    Add project files.

---
+++ SingletonDesignDemo/ConfigurationManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignDemo
+{
+    public sealed class ConfigurationManager
+    {
+        private static readonly ConfigurationManager Instance = new ConfigurationManager();
+
+        private ConfigurationManager()
+        {
+            LoadConfigurationSettings();
+        }
+
+        private Dictionary<string, string> _configuration = new Dictionary<string, string>();
+
+        public static ConfigurationManager GetConfigurationManagerInstance() { return Instance; }
+
+        private void LoadConfigurationSettings()
+        {
+            _configuration["Connectionstring"] = "Sample Connection";
+            _configuration["Loglevel"] = "1";
+        }
+
+        public string GetConfigurationSettings(string key)
+        {
+            if (_configuration.ContainsKey(key))
+                return _configuration[key];
+
+            return null;
+        }
+
+    }
+}
---
+++ SingletonDesignDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SingletonDesignDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConfigurationManager configurationManager = ConfigurationManager.GetConfigurationManagerInstance();
+
+            string connection = configurationManager.GetConfigurationSettings("Connectionstring");
+            string loglevel = configurationManager.GetConfigurationSettings("Loglevel");
+            Console.WriteLine("Connection string : " + connection);
+            Console.WriteLine("Loglevel : " + loglevel);
+            Console.Read();
+        }
+    }
+}
